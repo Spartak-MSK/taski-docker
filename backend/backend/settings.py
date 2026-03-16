347b6c7d73b32489ab3eda0901f21081a8cc5f92
Andrey Drobotov <endrudrobotov@mail.ru>
2026-03-16 07:11:02 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', '<your_domain>']
+ALLOWED_HOSTS = ['localhost', '123.123.123.123', 'taski.servebeer.com']
 
 
 # Application definition
